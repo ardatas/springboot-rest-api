cb3e86779cafdcd40eabb187cd53e6ba04f2709e
Arda <85059535+ardatas@users.noreply.github.com>
2026-02-23 17:00:59 +0300

    refactor: added lombok to reduce boilerplate code

--- src/main/java/com/ardatas/app/Engineer.java
+++ src/main/java/com/ardatas/app/Engineer.java
@@ -3,6 +3,7 @@ package com.ardatas.app;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.*;
 import org.jspecify.annotations.NonNull;
 
 import java.util.ArrayList;
@@ -15,60 +16,34 @@ import java.util.Objects;
 
 // Entities are passed directly to the api,
 // instead i need to use java records as DTOs
+
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 public class Engineer {
 
     // GENERATE AUTOMATIC ID
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    //this is a row
+    @Setter(AccessLevel.NONE) // prevent lombok from creating a setter for id
     private Integer id;
 
     // VALIDATION WITH NOT BLANK
     // DEPENDENCIES FOR VALIDATION: H2 SQL, SP Starter Validation (Bean)
-    // this is a row
     @NotBlank(message = "Name is mandatory")
     private String name;
     @NotBlank(message = "Tech stack is mandatory")
     private String techStack;
 
-    @Size(max=10)
     // defines one engineer -> many projects relationship
+    @Size(max=10)
     @OneToMany(mappedBy =  "engineer", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Project> projects = new ArrayList<>();
 
-
-    protected Engineer() {}
-
-    public Engineer (String name,
-                    String techStack) {
+    public Engineer(String name, String techStack) {
         this.name = name;
         this.techStack = techStack;
-    }
-
-
-    public Integer getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getTechStack() {
-        return techStack;
-    }
-
-    public void setTechStack(String techStack) {
-        this.techStack = techStack;
-    }
-
-    public List<Project> getProjects() {
-        return projects;
     }
 
     public void addProject(Project project) {
